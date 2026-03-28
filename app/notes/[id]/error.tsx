a0b32cff6372397fae25f8991c5487637017f206
Artur27 <arturfurman7@gmail.com>
2026-03-28 11:02:28 +0100

    Add error.tsx for /notes route

--- app/notes/[id]/error.tsx
+++ app/notes/[id]/error.tsx
@@ -1,18 +1,5 @@
 "use client";
 
-type Props = {
-  error: Error;
-  reset: () => void;
-};
-
-const Error = ({ error, reset }: Props) => {
-  return (
-    <div>
-      <h2>Помилка при завантаженні</h2>
-      <p>{error.message}</p>
-      <button onClick={reset}>Try Again</button>
-    </div>
-  );
-};
-
-export default Error;
+export default function NoteDetailsError({ error }: { error: Error }) {
+  return <p>Could not fetch note details. {error.message}</p>;
+}
